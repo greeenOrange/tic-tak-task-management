95693f1d02ed736a28f0b9da0ae35a0f396659e0
greeenOrange <gm1sadiq@gmail.com>
2023-09-08 21:36:11 +0600

    toodoo

--- src/Components/TaskManager/Modal/Modal.jsx
+++ src/Components/TaskManager/Modal/Modal.jsx
@@ -1,8 +1,36 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { ExclamationTriangleIcon } from '@heroicons/react/24/solid'
+import { v4 as uuidv4 } from 'uuid';
+uuidv4();
+function Modal({ showModal, setShowModal, formData, setFormData, setIsFormDataVisible }) {
+    const [inProcess, setInProcess] = useState([]);
+    const [formSubmissions, setFormSubmissions] = useState([]);
+    
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(
+            {    ...formData,
+                [name]: value,
+                id: uuidv4(), task: inProcess,
+                completed: false, isEditiong: false,
+                
+            }
+            )
+            setIsFormDataVisible(true)
+        console.log(formData);
+    };
 
-function Modal({ showModal, setShowModal }) {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+  const newCard = { ...formData, id: uuidv4() };
+  const existingData = JSON.parse(localStorage.getItem('formData')) || [];
+  const updatedData = [...existingData, newCard];
+  localStorage.setItem('formData', JSON.stringify(updatedData));
+
+  setShowModal(false);
+    };
+
     return (
         <Transition.Root show={showModal} as={Fragment}>
             <Dialog as="div" className="relative z-10" onClose={setShowModal}>
@@ -30,42 +58,58 @@ function Modal({ showModal, setShowModal }) {
                             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         >
                             <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                                    <div className="mt-3 flex flex-col sm:gap-4 sm:ml-4 sm:mt-0 w-full">
-                                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                                            <input type="text" placeholder="Type Title" className="input input-bordered w-full" />
-                                        </Dialog.Title>
-                                        <div className="mt-2">
-                                            <textarea className="textarea textarea-bordered w-full" placeholder="Bio"></textarea>
+                                <form action="" onSubmit={handleSubmit}>
+                                    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                        <div className="mt-3 flex flex-col sm:gap-4 sm:ml-4 sm:mt-0 w-full">
+                                            <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                                                <input type="text"
+                                                    placeholder="Type Title"
+                                                    name="title"
+                                                    value={formData?.title}
+                                                    onChange={handleChange}
+                                                    className="input input-bordered w-full" />
+                                            </Dialog.Title>
+                                            <div className="mt-2">
+                                                <textarea
+                                                    name="discription"
+                                                    value={formData?.discription}
+                                                    onChange={handleChange}
+                                                    className="textarea textarea-bordered w-full" placeholder="Description"></textarea>
+                                            </div>
+                                            <input type="date"
+                                                name="date"
+                                                value={formData?.date}
+                                                onChange={handleChange} />
+                                            <select
+                                                name="priority"
+                                                value={formData?.priority}
+                                                onChange={handleChange}
+                                                className="select select-bordered w-full max-w-xs">
+                                                <option disabled selected>low</option>
+                                                <option value="low">Low</option>
+                                                <option value="medium">Medium</option>
+                                                <option value="high">High</option>
+                                                <option value="Now">Now!</option>
+                                            </select>
                                         </div>
-                                        <input type="date" name="" id="" />
-                                        <select className="select select-bordered w-full max-w-xs">
-                                            <option disabled selected>Priority level</option>
-                                            <option>Low</option>
-                                            <option>Medium</option>
-                                            <option>High</option>
-                                            <option>Now!</option>
-                                        </select>
+
                                     </div>
-
-                                </div>
-                                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                <button
-                                        type="button"
-                                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:ml-3 sm:w-auto"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        submit
-                                    </button>
-                                    <button
-                                        type="button"
-                                        className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:w-auto"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Cancel
-                                    </button>
-
-                                </div>
+                                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                        <button
+                                            type="submit"
+                                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:ml-3 sm:w-auto"
+                                        >
+                                            submit
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:w-auto"
+                                            onClick={() => setShowModal(false)}
+                                        >
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </form>
                             </Dialog.Panel>
                         </Transition.Child>
                     </div>
--- src/Components/TaskManager/TaskManager.jsx
+++ src/Components/TaskManager/TaskManager.jsx
@@ -1,9 +1,17 @@
-import { Fragment } from 'react'
 import './TaskManager.css';
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, MinusIcon, PlusIcon, XMarkIcon } from '@heroicons/react/24/solid'
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 import ToDoCard from './ToDoCard/ToDoCard';
+import { useState } from 'react';
+
 function TaskManager() {
+  const [formData, setFormData] = useState({
+    title: '',
+    date: '',
+    discription: '',
+    priority: 'low',
+    task: 'inProcess',
+});
   return (
     <section className="task-management">
         <div className="container">
@@ -31,23 +39,6 @@ function TaskManager() {
                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                     alt="Your Company"
                   />
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {/* {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))} */}
-                  </div>
                 </div>
               </div>
             </div>
@@ -94,7 +85,7 @@ function TaskManager() {
 
               {/* Product grid */}
               <div className="lg:flex lg:columns px-4">
-                    <ToDoCard />
+                    <ToDoCard formData={formData} setFormData={setFormData}/>
                 </div>
             </div>
         </div>
